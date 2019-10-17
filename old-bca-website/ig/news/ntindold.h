494ac1d258abd2907f1634ccd14df9c62fd06cd5
Richard Cooper <richardiancooper@gmail.com>
2019-10-17 15:44:02 +0100

    Archiving old (pre-2009) BCA website

---
+++ old-bca-website/ig/news/ntindold.h
@@ -0,0 +1,113 @@
+<HTML>
+<HEAD>
+
+<TITLE>Industrial Group Newsletter techniques index</TITLE>
+</HEAD>
+<BODY LINK="#0000ff" VLINK="#800080" BGCOLOR="#ffffff">
+<h1>Index to Techniques articles</h1>
+This begins with a subject index, <a href="#Auth">click here</a>
+ if you prefer the Author index.
+Indexed in here Jan97 Aug 97 Jan98 Jul98 Jan99  
+  better UPDATE CNews.ICDD.html with the new subject stuff from these 
+<hr>
+<h2>Subject index</h2>
+<ul>
+  <li>Camera techniques
+    <ul>
+      <li><a href="N98T1.html#Camera">Flat plate </a> and 2 dimensional
+- The overlooked Tools! - Bruce Fox and Kath Clay <strong>Jan 98</strong>
+      <li><a href="N98T2.html#ttcam">Wallace-Ward (Marconi) thin film </a> 
+camera - Bruce Fox <strong>Jul 98</strong>
+    </ul> 
+  <li><a href="N99T1.html#medapp">Diffraction reaches the parts other medical tests cannot reach</a>- Keith Rogers <strong> Jan 99</strong>
+  <li><a href="N97T2.html#darts">Darts</A> - A new service for Industry 
+        at the SRS - Steve Maginn <strong>Aug 97</strong>
+  <li><a href="N97T2.html#epdic5">5th European Powder Diffraction Meeting at
+ Parma (EPDIC-5)</a> - Steve Norval <strong>Aug 97</strong>
+  <li>European Standards Initiative in <A HREF="N97T2.html#EUSTD">XRPD</a>
+ <strong>Aug 97</strong> - Steve Norval <strong>Aug 97</strong>
+  <li> Setting up <a href="N97T1.html#help">Proportional Counters </a> 
+- Trevor Carter <strong>Jan 97</strong>   
+  <li><A HREF="N97T2.html#refstd"> Reference Standards</A> for routine 
+       diffractometer checks <strong>Aug 97</strong>
+  <li>Intensity Round Robin
+    <ul>
+      <li> <a href="N98T2.html#rrguide">Self test guide </a> - Dave Taylor 
+        <strong>Jul 98 </strong>   
+      <li> <a href="N97T1.html#rrob"> Update</a> - Dave Taylor <strong>Jan 97
+     </strong>   
+      <li><A HREF="N97T2.html#RNDROB">Update </A> - Dave Taylor 
+<strong>Aug 97</strong>
+     </ul>
+  <li>Rietveld <a href="N98T1.html#Rvalues">'R' values </a>discussed -
+ Keith Rogers <strong>Jan 98</strong> 
+  <li><a href="N98T1.html#Holders">Sample Holders</a> - Tim Hyde 
+ <strong>Jan 98</strong> 
+  <LI><a href="N97T1.html#siginfo">Special Interest Groups</A> Member Survey
+ - Steve Norval <strong>Jan 97</strong>
+  <li>Spurious <a href="N97T1.html#tape">Peaks from Sellotape </a>
+- Bruce Fox <strong>Jan 97</strong>
+</ul>
+<a name="Auth"><hr></a>
+<h2>Author index</h2>
+Alphabetic by first letter of surname.<br>
+<ul> 
+  <li>Trevor Carter
+    <ul>
+      <li> Setting up <a href="N97T1.html#help">Proportional Counters </a> 
+  Jan 97
+    </ul>
+  <li>Kath Clay
+    <ul>
+      <li><a href="N98T1.html#Clay">Camera Techniques </a> Jan 98
+    </ul>
+  <li>Bruce Fox
+    <ul>
+      <li>Camera Techniques
+       <ul>
+         <li><a href="N98T1.html#Camera">Flat plate </a> - Jan 98
+         <li><a href="N98T2.html#ttcam">Wallace-Ward (Marconi) thin film </a> 
+camera - Jul 98
+       </ul>
+   <li>Spurious <a href="N97T1.html#tape">Peaks from Sellotape </a> Jan 97  
+ </ul>
+  <li>Tim Hyde
+    <ul>
+      <li><a href="N98T1.html#Holders">Sample Holders</a> - Jan 98
+    </ul>
+  <li>Steve Maginn
+    <ul>
+      <li><a href="N97T2.html#darts">DARTS </a> service at the SRS - Aug 97
+    </ul>
+  <li>Steve Norval
+    <ul>
+      <li><a href="N97T2.html#epdic5">EPDIC5 </a> 5th European Powder 
+Diffraction Meeting at Parma - Aug 97
+      <li>European Standards Initiative in <a href="N97T2.html#EUSTD">
+ Powder Diffraction Methods </a> 
+      <li><a href="N97T2.html#refstd">Reference Standards</A> for routine 
+       diffractometer checks  - Aug 97
+      <LI><a href="N97T1.html#siginfo">Special Interest Groups</A> 
+Member Survey Jan 97 
+    </ul>
+  <li>Keith Rogers
+     <ul>
+      <li>Reitveld <a href="N98T1.html#Rvalues">'R' values </a>discussed. 
+Jan 98 
+     <li><a href="N99T1.html#medapp">Diffraction reaches the parts other medical tests cannot reach</a>- Jan 99
+     </ul>
+  <li>Dave Taylor  
+    <ul>
+      <li>Round Robin  <a href="N98T2.html#rrguide">Self test guide </a> Jul 98
+      <li>Intensity <a href="N97T1.html#rrob">Round Robin Update</a> Jan 97 
+      <li>Intensity <a href="N97T2.html#RNDROB">Round Robin Update</a> - Aug 97
+    </ul>
+     </ul>
+</ul>
+<hr>
+ Page last updated <em> 1 Dec 98</em> by the  <ADDRESS>BCA Home page WebMaster 
+<A HREF="mailto:BCA@ISISE.RL.AC.UK">BCA@ISISE.RL.AC.UK</A> </ADDRESS> 
+<p><A HREF="../ig.html"> <IMG SRC="../../images/gball.gif" ALIGN="MIDDLE"
+ALT="<--"> Click here to return to IG Contents </A> </p>
+</BODY>
+</HTML>
